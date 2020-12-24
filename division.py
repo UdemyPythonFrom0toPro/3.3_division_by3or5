4235f096ed2324bcdad36ed9b0d4248ccf3f69d6
Firuz Amirov <59014178+firuzamiroff@users.noreply.github.com>
2020-12-24 04:53:37 +0400

    Update division.py

--- division.py
+++ division.py
@@ -2,6 +2,8 @@ import sys
 
 limit = int(sys.argv[1])
 
+# total_sum = sum([x for x in range(limit + 1)  if x % 3 == 0 or x % 5 == 0])
+
 total_sum = 0
 for x in range(limit + 1):
         if x % 3 == 0 or x % 5 == 0:
